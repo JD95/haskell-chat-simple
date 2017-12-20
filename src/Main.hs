98d6ce8a8d7a392b1322cc5ad0deadd615526e26
Jeff <jeffreydwyer95@hotmail.com>
2017-12-20 15:32:59 -0800

    Sever and clients mostly done

--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,8 @@
 {-# LANGUAGE TypeApplications  #-}
 module Main where
 
+import           Control.Concurrent
+import qualified Data.ByteString     as B
 import           Lens.Micro.Platform
 import           Network.Simple.TCP
 import           Options.Applicative
@@ -32,18 +34,67 @@ main = do
   options <- execParser optsInfo
   print (asClient options)
 
+readClientMessages :: Socket -> Chan ByteString -> IO ()
+readClientMessages sock chan =
+  forever $ do
+    message <- B.concat <$> recvUntilEnd sock 1024
+    when (message /= "quit\r\n\r\n") $ do
+      writeChan chan message
+      readClientMessages sock chan
+    pure ()
+
+sendClientMessages :: Socket -> Chan ByteString -> IO ()
+sendClientMessages sock chan =
+  forever $ do
+    message <- readChan chan
+    send sock message
+
 server :: IO ()
 server = do
   print "running as server..."
+  messages <- newChan @ ByteString
   serve (Host "127.0.0.1") "8000" $ \(sock, remoteAddr) -> do
     putStrLn @Text $ "Connection established from " <> show remoteAddr
+    ms <- dupChan messages
+    writer <- forkIO $ sendClientMessages sock ms
+    readClientMessages sock ms
+    killThread writer
+    putStrLn @Text $ "closing connection for " <> show remoteAddr
+
+recvUntilEnd :: Socket -> Int -> IO [ByteString]
+recvUntilEnd s i = do
+  r <- recv s i
+  case r of
+    Just r' ->
+      if "\r\n\r\n" `B.isSuffixOf` r'
+        then pure [r']
+        else (r' :) <$> recvUntilEnd s i
+    Nothing -> pure []
+
+messageEntry :: IO ()
+messageEntry = do
+  forever $ print =<< getLine
+
+sendMessage :: Socket -> ByteString -> IO ()
+sendMessage sock m = send sock (m <> "\r\n\r\n")
+
+recieveServerMessages :: Socket -> IO ()
+recieveServerMessages sock =
+  forever $ do
+    message <- B.concat <$> recvUntilEnd sock 1024
+    print message
 
 client :: IO ()
 client = do
   print "running as client..."
   putStr @Text "server ip: "
-  ip <- toS <$> getLine
+  ip <- pure "127.0.0.1" -- toS <$> getLine
   putStr @Text "server port: "
-  port <- toS <$> getLine
+  port <- pure "8000" -- toS <$> getLine
   connect ip port $ \(sock, remoteAddr) -> do
     putStrLn @Text $ "Connection established to " <> show remoteAddr
+    forkIO $ recieveServerMessages sock
+    forever $ do
+      putStr @Text "> "
+      message <- getLine
+      sendMessage sock (toS message)
